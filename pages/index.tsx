189de9eb0475a26f690d6045d6c6147fc9c05f2e
afzal saiyed <saiyedafzalgz@gmail.com>
2021-08-04 01:32:52 +0530

    added some default styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,41 @@
-import Head from "next/head";
-import Image from "next/image";
 import MainLayout from "../components/common/MainLayout";
 import MetaHead from "../components/common/Head";
-
-export default function Home() {
-    const description =
+import { NextPage } from 'next'
+const Home: NextPage = () => {
+    const description:string =
         "CARTDROP is the leading ecommerce platform in India. CARTDROP is the best open-source eCommerce shopping cart solution. Cartdrop is free, and it is the most popular Django eCommerce platform.";
     return (
-        <MainLayout>
-            <>
-                <MetaHead
-                    title="Home"
-                    description={description}
-                    currentUrl="/"
-                    keywords="ecommerce, opensource, django, django rest framework, redis, postgresql, nextjs, typescript, tailwing, best, ecommerce, platform, india, 2021, fullstack"
-                />
+        <>
+            <MetaHead
+                title="Home"
+                description={description}
+                currentUrl="/"
+                keywords="ecommerce, opensource, django, django rest framework, redis, postgresql, nextjs, typescript, tailwing, best, ecommerce, platform, india, 2021, fullstack"
+            />
+            <MainLayout>
                 <div>
-                    <div>
+                    <div className="text-main">
                         <h1>heading 1</h1>
-                        <h1>heading 2</h1>
-                        <h1>heading 3</h1>
-                        <h1>heading 4</h1>
-                        <h1>heading 5</h1>
-                        <h1>heading 6</h1>
+                        <h2>heading 2</h2>
+                        <h3>heading 3</h3>
+                        <h4>heading 4</h4>
+                        <h5>heading 5</h5>
+                        <h6>heading 6</h6>
                     </div>
                     <div>
                         <p>Paragraph</p>
                         <a href="">Link</a>
+                        <br />
                         <span>Span</span>
                     </div>
+                    <br />
                     <div>
                         <h1>Inputs</h1>
                     </div>
                 </div>
-            </>
-        </MainLayout>
+            </MainLayout>
+        </>
     );
 }
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,46 @@
 module.exports = {
+    mode: 'jit',
     purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                main: "rgb(45,61,138)",
+                secondary: "rgb(28, 117, 244)",
+                background: "rgb(255, 255, 255)",
+                text: "rgb(57, 58, 67)",
+            },
+            fontFamily: {
+                poppins: [
+                    "Poppins",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    "Segoe UI",
+                    "Roboto",
+                    "Oxygen",
+                    "Ubuntu",
+                    "Cantarell",
+                    "Fira Sans",
+                    "Droid Sans",
+                    "Helvetica Neue",
+                    "sans-serif",
+                ],
+                roboto: [
+                    "Roboto",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    "Segoe UI",
+                    "Roboto",
+                    "Oxygen",
+                    "Ubuntu",
+                    "Cantarell",
+                    "Fira Sans",
+                    "Droid Sans",
+                    "Helvetica Neue",
+                    "sans-serif",
+                ]
+            }
+        },
     },
     variants: {
         extend: {},
