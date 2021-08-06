c88bf4ad1b62fdfd6508664afa09ed8ac73de0d7
afzal saiyed <saiyedafzalgz@gmail.com>
2021-08-07 01:15:50 +0530

    changed hero image

--- components/Home/Hero/index.tsx
+++ components/Home/Hero/index.tsx
@@ -2,8 +2,10 @@ import { NextPage } from 'next'
 
 const Hero: NextPage = () => {
     return (
-        <div className="bg-hero-image w-full h-hero bg-no-repeat bg-cover bg-clip-content bg-center">
-            
+        <div className="bg-hero-image w-full h-hero bg-no-repeat bg-cover bg-clip-content bg-center flex justify-center items-center">
+            <div className="bg-white py-10 px-14 skew-x-hero-outer bg-opacity-60">
+                <h1 className="skew-x-hero-inner text-text">25% OFF ON FIRST ORDER USE COUPON FIRST25</h1>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -53,13 +53,18 @@ module.exports = {
                 "drop-down": "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px",
             },
             backgroundImage: theme => ({
-                'hero-image': "url('https://res.cloudinary.com/corecare/image/upload/c_scale,w_1920/v1628273300/cartdrop/pexels-andrea-piacquadio-3769747_karaf4.jpg')"
+                'hero-image': "linear-gradient(3deg, rgba(45,61,138,0.5) 0%, rgba(45,61,138,0.5) 100%), url('https://res.cloudinary.com/corecare/image/upload/c_scale,w_1920/v1628278383/cartdrop/cropped_hero_unoptimized_mvo43h.jpg')",
+
             }),
             height: {
-                'hero': '600px  '
+                'hero': '700px  '
             },
             screens: {
                 "ipad": "992px",
+            },
+            skew: {
+                'hero-outer': "35deg",
+                'hero-inner': "-35deg"
             }
         },
     },
