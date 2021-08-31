5538c94baa3dd9e511aa335f222d2287e2ef31c9
AFzal Saiyed <saiyedafzalgz@gmail.com>
2021-09-01 01:31:34 +0530

    made product slider fully reusable

--- components/Home/FeaturedProducts.tsx
+++ components/Home/FeaturedProducts.tsx
@@ -4,7 +4,7 @@ import ProductList from "../common/ProductSlider";
 import { FeaturedProductType } from "../../shared/types";
 import SmallButtonLink from "../common/buttons/SmallButtonLink";
 import SliderControl from "../common/SliderControl";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 interface IProps {
     featuredProducts: FeaturedProductType[];
@@ -14,74 +14,22 @@ const FeaturedProducts: NextPage<IProps> = ({ featuredProducts }) => {
     const slideableContainerRef = useRef<HTMLDivElement | null>(null);
     const sliderItemRef = useRef<HTMLDivElement | null>(null);
     const currentSliderPosition = useRef<number>(0);
-    const sliderLeftControlRef = useRef<HTMLDivElement | null>(null);
-    const sliderRightControlRef = useRef<HTMLDivElement | null>(null);
 
-    const goLeft = () => {
-        if (slideableContainerRef.current && sliderItemRef.current && sliderLeftControlRef.current && sliderRightControlRef.current) {
-            let stopPosition;
-            if (window.innerWidth > 992) {
-                if (window.innerWidth <= 1600) {
-                    stopPosition = -(currentSliderPosition.current,
-                    sliderItemRef.current?.clientWidth * (featuredProducts.length - 3) + 15);
-                } else {
-                    stopPosition = -(currentSliderPosition.current,
-                    sliderItemRef.current?.clientWidth * (featuredProducts.length - 4) + 15);
-                }
-                if (currentSliderPosition.current > stopPosition) {
-                    console.log(currentSliderPosition.current, stopPosition + (sliderItemRef.current.clientWidth - 30))
-                    sliderLeftControlRef.current.classList.remove("bg-gray-400");
-                    sliderLeftControlRef.current.classList.remove("text-gray-200");
-                    sliderLeftControlRef.current.classList.remove("cursor-not-allowed");
-                    sliderLeftControlRef.current.classList.add("bg-secondary");
-                    sliderLeftControlRef.current.classList.add("hover:bg-main");
-                    sliderLeftControlRef.current.classList.add("text-white");
-                    sliderLeftControlRef.current.classList.add("cursor-pointer");
-
-                    currentSliderPosition.current = currentSliderPosition.current - sliderItemRef.current.clientWidth - 15;
-                    slideableContainerRef.current.style.transform = `translate3d(${currentSliderPosition.current}px, 0px, 0px)`;
-                } 
-                
-                if(currentSliderPosition.current <= stopPosition) {
-                    sliderRightControlRef.current.classList.remove("bg-secondary");
-                    sliderRightControlRef.current.classList.remove("hover:bg-main");
-                    sliderRightControlRef.current.classList.remove("text-white");
-                    sliderRightControlRef.current.classList.remove("cursor-pointer");
-                    sliderRightControlRef.current.classList.add("bg-gray-400");
-                    sliderRightControlRef.current.classList.add("text-gray-200");
-                    sliderRightControlRef.current.classList.add("cursor-not-allowed");
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 992){
+                if (slideableContainerRef.current){
+                    currentSliderPosition.current = 0
+                    slideableContainerRef.current.style.transform = "translate3d(0px, 0px, 0px)"
                 }
             }
         }
-    };
-    const goRight = () => {
-        if (slideableContainerRef.current && sliderItemRef.current && sliderRightControlRef.current && sliderLeftControlRef.current) {
-            if (window.innerWidth > 992) {
-                if (currentSliderPosition.current < 0) {
-                    sliderRightControlRef.current.classList.remove("bg-gray-400");
-                    sliderRightControlRef.current.classList.remove("text-gray-200");
-                    sliderRightControlRef.current.classList.remove("cursor-not-allowed");
-                    sliderRightControlRef.current.classList.add("bg-secondary");
-                    sliderRightControlRef.current.classList.add("hover:bg-main");
-                    sliderRightControlRef.current.classList.add("text-white");
-                    sliderRightControlRef.current.classList.add("cursor-pointer");
-
-                    currentSliderPosition.current = currentSliderPosition.current + sliderItemRef.current.clientWidth + 15;
-                    slideableContainerRef.current.style.transform = `translate3d(${currentSliderPosition.current}px, 0px, 0px)`;
-                } 
-                
-                if (currentSliderPosition.current >= (0 - sliderItemRef.current.clientWidth + 15)) {
-                    sliderLeftControlRef.current.classList.remove("bg-secondary");
-                    sliderLeftControlRef.current.classList.remove("hover:bg-main");
-                    sliderLeftControlRef.current.classList.remove("text-white");
-                    sliderLeftControlRef.current.classList.remove("cursor-pointer");
-                    sliderLeftControlRef.current.classList.add("bg-gray-400");
-                    sliderLeftControlRef.current.classList.add("text-gray-200");
-                    sliderLeftControlRef.current.classList.add("cursor-not-allowed");
-                }
-            }
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
         }
-    };
+    }, [])
+    
 
     return (
         <Section title="FEATURED">
@@ -89,12 +37,7 @@ const FeaturedProducts: NextPage<IProps> = ({ featuredProducts }) => {
                 <SmallButtonLink to="/products/featured" text="View All" />
             </div>
             <div className="absolute z-10 right-0 top-2 hidden ipad:block">
-                <SliderControl
-                    goLeft={goLeft}
-                    goRight={goRight}
-                    rightControlRef={sliderRightControlRef}
-                    leftControlRef={sliderLeftControlRef}
-                />
+                <SliderControl slideableContainerRef={slideableContainerRef} sliderItemRef={sliderItemRef} currentSliderPosition={currentSliderPosition} productsLength={featuredProducts.length}/>
             </div>
             <ProductList featuredProducts={featuredProducts} slideableContainerRef={slideableContainerRef} sliderItemRef={sliderItemRef} />
         </Section>
--- components/common/SliderControl.tsx
+++ components/common/SliderControl.tsx
@@ -1,13 +1,82 @@
 import { NextPage } from "next";
 import { MutableRefObject } from "react";
+import { useRef } from 'react'
 
 interface SliderControlProps {
-    goLeft: () => void;
-    goRight: () => void;
-    rightControlRef: MutableRefObject<HTMLDivElement | null>;
-    leftControlRef: MutableRefObject<HTMLDivElement | null>;
+    slideableContainerRef: MutableRefObject<HTMLDivElement | null>;
+    sliderItemRef: MutableRefObject<HTMLDivElement | null>;
+    currentSliderPosition: MutableRefObject<number>;
+    productsLength: number;
+
 }
-const SliderControl: NextPage<SliderControlProps> = ({ goLeft, goRight, rightControlRef, leftControlRef }) => {
+const SliderControl: NextPage<SliderControlProps> = ({ slideableContainerRef, sliderItemRef, currentSliderPosition, productsLength }) => {
+    const rightControlRef = useRef<HTMLDivElement | null>()
+    const leftControlRef = useRef<HTMLDivElement | null>()
+    const goLeft = () => {
+        if (slideableContainerRef.current && sliderItemRef.current && rightControlRef.current && leftControlRef.current) {
+            let stopPosition;
+            if (window.innerWidth >= 992) {
+                if (window.innerWidth <= 1600) {
+                    stopPosition = -(currentSliderPosition.current,
+                    sliderItemRef.current?.clientWidth * (productsLength - 3) + 15);
+                } else {
+                    stopPosition = -(currentSliderPosition.current,
+                    sliderItemRef.current?.clientWidth * (productsLength - 4) + 15);
+                }
+                if (currentSliderPosition.current > stopPosition) {
+                    console.log(currentSliderPosition.current, stopPosition + (sliderItemRef.current.clientWidth - 30))
+                    leftControlRef.current.classList.remove("bg-gray-400");
+                    leftControlRef.current.classList.remove("text-gray-200");
+                    leftControlRef.current.classList.remove("cursor-not-allowed");
+                    leftControlRef.current.classList.add("bg-secondary");
+                    leftControlRef.current.classList.add("hover:bg-main");
+                    leftControlRef.current.classList.add("text-white");
+                    leftControlRef.current.classList.add("cursor-pointer");
+
+                    currentSliderPosition.current = currentSliderPosition.current - sliderItemRef.current.clientWidth - 15;
+                    slideableContainerRef.current.style.transform = `translate3d(${currentSliderPosition.current}px, 0px, 0px)`;
+                } 
+                
+                if(currentSliderPosition.current <= stopPosition) {
+                    rightControlRef.current.classList.remove("bg-secondary");
+                    rightControlRef.current.classList.remove("hover:bg-main");
+                    rightControlRef.current.classList.remove("text-white");
+                    rightControlRef.current.classList.remove("cursor-pointer");
+                    rightControlRef.current.classList.add("bg-gray-400");
+                    rightControlRef.current.classList.add("text-gray-200");
+                    rightControlRef.current.classList.add("cursor-not-allowed");
+                }
+            }
+        }
+    };
+    const goRight = () => {
+        if (slideableContainerRef.current && sliderItemRef.current && rightControlRef.current && leftControlRef.current) {
+            if (window.innerWidth >= 992) {
+                if (currentSliderPosition.current < 0) {
+                    rightControlRef.current.classList.remove("bg-gray-400");
+                    rightControlRef.current.classList.remove("text-gray-200");
+                    rightControlRef.current.classList.remove("cursor-not-allowed");
+                    rightControlRef.current.classList.add("bg-secondary");
+                    rightControlRef.current.classList.add("hover:bg-main");
+                    rightControlRef.current.classList.add("text-white");
+                    rightControlRef.current.classList.add("cursor-pointer");
+
+                    currentSliderPosition.current = currentSliderPosition.current + sliderItemRef.current.clientWidth + 15;
+                    slideableContainerRef.current.style.transform = `translate3d(${currentSliderPosition.current}px, 0px, 0px)`;
+                } 
+                
+                if (currentSliderPosition.current >= (0 - sliderItemRef.current.clientWidth + 15)) {
+                    leftControlRef.current.classList.remove("bg-secondary");
+                    leftControlRef.current.classList.remove("hover:bg-main");
+                    leftControlRef.current.classList.remove("text-white");
+                    leftControlRef.current.classList.remove("cursor-pointer");
+                    leftControlRef.current.classList.add("bg-gray-400");
+                    leftControlRef.current.classList.add("text-gray-200");
+                    leftControlRef.current.classList.add("cursor-not-allowed");
+                }
+            }
+        }
+    };
     return (
         <div className="flex space-x-2">
             <div
