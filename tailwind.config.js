26bd347fc1378c9607ddc672a9331b18e978a480
Afzal <afzal.saiyed@3pillarglobal.com>
2021-12-12 14:02:37 +0530

    upgraded tailwingcss to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: "jit",
-    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-    darkMode: false, // or 'media' or 'class'
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    // darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             colors: {
@@ -88,8 +88,8 @@ module.exports = {
             },
         },
     },
-    variants: {
-        extend: {},
-    },
+    // variants: {
+    //     extend: {},
+    // },
     plugins: [],
 };
