ee8e6f815b035236b52117d22e3a04b2f65d3081
Afzal <afzal.saiyed@3pillarglobal.com>
2021-12-12 18:26:35 +0530

    added product list layout

---
+++ components/categories/ProductList/Products/ProductItem.tsx
@@ -0,0 +1,9 @@
+const ProductItem = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ProductItem
---
+++ components/categories/ProductList/Products/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const Products: NextPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Products
