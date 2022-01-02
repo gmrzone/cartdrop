6ae679f1468f17f963b20216639eab31d686038e
Afzal <afzal.saiyed@3pillarglobal.com>
2022-01-02 14:45:01 +0530

    added custom checkbox

--- components/common/inputs/Checkbox.tsx
+++ components/common/inputs/Checkbox.tsx
@@ -2,8 +2,12 @@ import { NextPage } from "next";
 
 const Checkbox: NextPage = () => {
     return (
-        <div>
-            <input type="checkbox" className="appearance-none h-[1.85rem] w-[1.85rem] rounded-[0.200rem] border border-main" />
+        <div className="relative">
+            <input
+                type="checkbox"
+                className="peer appearance-none h-[1.85rem] w-[1.85rem] rounded-[0.200rem] border border-main cursor-pointer relative z-50"
+            />
+            <i className="far fa-check text-main text-xl h-full w-full top-0 left-0 absolute peer-checked:text-opacity-100 text-opacity-0 z-40 text-center" />
         </div>
     );
 };
