426623f212553d9de1dbe79920c407717590e832
Afzal <saiyedafzalgz@gmail.com>
2022-02-15 16:57:23 +0530

    fixed github action

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,20 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 
-
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  globals: {
-    "ts-jest": {
-      tsconfig: "./tsconfig.jest.json"
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    globals: {
+        "ts-jest": {
+            tsconfig: "./tsconfig.jest.json",
+        },
     },
-  },
-  setupFilesAfterEnv: ['./jest.setup.ts'],
+    setupFilesAfterEnv: ["./jest.setup.ts"],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 70,
+        },
+    },
 };
