e2473a863586711e10e2846d25ca3842d0e285e2
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-08-13 14:20:05 +0530

    fixed type error

--- components/Home/TodeysDeal/data.ts
+++ components/Home/TodeysDeal/data.ts
@@ -1,4 +1,12 @@
-import TVImage from "../../../public/TV.jpg";
+import TVImage from '../../../public/TV.jpg';
+
+// TODO: Remove this when nexj js fixes type error on StaticImageData
+type StaticImageData = {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+};
 
 type TodaysDealT = {
     name: string;
@@ -10,10 +18,10 @@ type TodaysDealT = {
 };
 
 export const todaysDeal: TodaysDealT = {
-    name: "Samsung 125cm (50 Inches) Crystal 4K series Ultra HD Smart LET TV",
-    mrp: "55,999",
-    off: "28% off",
-    price: "45,999",
+    name: 'Samsung 125cm (50 Inches) Crystal 4K series Ultra HD Smart LET TV',
+    mrp: '55,999',
+    off: '28% off',
+    price: '45,999',
     offer_ends: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 1),
     image: TVImage,
 };
